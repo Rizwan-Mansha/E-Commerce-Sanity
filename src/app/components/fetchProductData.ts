d1f536ce5fdc121635bd289aa0ca9de7f0f0e66f
Rizwan Mansha <104033300+Rizwan-Mansha@users.noreply.github.com>
2023-10-06 16:03:08 +0500

    getProductData type set

--- src/app/components/fetchProductData.ts
+++ src/app/components/fetchProductData.ts
@@ -1,3 +1,4 @@
+import { groq } from "next-sanity";
 import { client } from "../../../sanity/lib/client";
 import { Image as IIamge } from "sanity";
 
@@ -13,7 +14,7 @@ export interface IProduct {
 }
 
 const getProductData = async () => {
-  const res = await client.fetch(`*[_type=="product"]{
+  const res = await client.fetch(groq`*[_type=="product"]{
   _id,
   price,
   title,
